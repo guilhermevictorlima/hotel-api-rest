e4edf3b689b8450229ed7f281fb0f67fb9956e55
Guilherme Lima <guilhermevictorrocha@gmail.com>
2023-12-10 22:56:53 -0300

    chore: configurando envio de e-mails

---
+++ src/main/java/com/github/guilhermevictorlima/hotelapi/EmailConfig.java
@@ -0,0 +1,34 @@
+package com.github.guilhermevictorlima.hotelapi;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class EmailConfig {
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        Dotenv dotenv = Dotenv.load();
+
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername(dotenv.get("EMAIL"));
+        mailSender.setPassword(dotenv.get("EMAIL_SENHA"));
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+
+}
